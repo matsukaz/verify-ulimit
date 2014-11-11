997ab03711299f6ea27d4522c129f5e29fd15152
matsukaz <matsukaz@gmail.com>
2014-11-11 16:47:11 +0900

    log process.env.SHELL value

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var app = express();
 var ulimit = require('ulimit');
 var fs = require('fs');
 
-process.env.SHELL = process.env.SHELL || '/bin/bash';
+process.env.SHELL = process.env && process.env.SHELL || '/bin/bash';
+console.log(process.env.SHELL);
 
 app.get('/', function (req, res) {
   ulimit(function(err, result) {
