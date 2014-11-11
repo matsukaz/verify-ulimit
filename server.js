76c5743992c07cb85eecfdd66089f033c5b05b0f
matsukaz <matsukaz@gmail.com>
2014-11-11 15:40:48 +0900

    set /bin/bash if process.env.SHELL does not exists

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var ulimit = require('ulimit');
 var fs = require('fs');
 
+process.env.SHELL = process.env.SHELL || '/bin/bash';
+
 app.get('/', function (req, res) {
   ulimit(function(err, result) {
     if (err) {
